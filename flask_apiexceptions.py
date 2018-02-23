f1f52b2156c54b4ede63cc44fc410afef7c5dd1d
Joel Perras <joel@nerderati.com>
2018-02-23 15:36:58 -0500

    1.1.0 release.

--- flask_apiexceptions.py
+++ flask_apiexceptions.py
@@ -13,7 +13,7 @@ Base API exception classes.
 from __future__ import absolute_import
 from __future__ import print_function
 
-__version_info__ = ('1', '0', '6')
+__version_info__ = ('1', '1', '0')
 __version__ = '.'.join(__version_info__)
 __author__ = 'Joel Perras'
 __copyright__ = '(c) 2018 Fictive Kin, LLC'
